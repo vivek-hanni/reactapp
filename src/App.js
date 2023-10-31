d86b456f3dbc25d7467d299cb1fdfad468d39b7a
vivek hanni <vivek279.hanni@gmail.com>
2023-10-31 11:28:53 +0530

    new addings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import HelloWorldList from './HelloWorldList';
 
 const App = () => {
+  const [words, setWords] = useState([]);
+  
+  useEffect(() => {
+    // Simulate fetching words from an API or some other data source
+    // For this example, we'll use a simple array of words.
+    const wordsArray = ['Hello', 'World', 'React', 'Example'];
+    
+    // Update the state with the words
+    setWords(wordsArray);
+  }, []);
+
   return (
-  <div className="App">
-  <HelloWorldList />
-  </div>
+    <div className="App">
+      {words.map((word, index) => (
+        <p key={index}>{word}</p>
+      ))}
+      <HelloWorldList />
+    </div>
   );
 };
 
 export default App;
+
+// import React from 'react';
+// import './App.css';
+// import HelloWorldList from './HelloWorldList';
+
+// const App = () => {
+//   return (
+//   <div className="App">
+//   <HelloWorldList />
+//   </div>
+//   );
+// };
+
+// export default App;
